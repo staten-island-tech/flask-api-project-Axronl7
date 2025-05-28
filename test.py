0a674ab9c66f3661a432f364af90475e720ae726
Axronl7 <Aaronliu657@gmail.com>
2025-05-28 10:14:37 -0400

    hm

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import requests
-response = requests.get("https://pokeapi.co/api/v2/pokemon?limit=150")
+response = requests.get("https://pokeapi.co/api/v2/pokemon?limit=1")
 data = response.json()
 pokemon_list = data['results']
 for pokemon in pokemon_list:
-    print(pokemon['url'])
+    print(pokemon['name'])
