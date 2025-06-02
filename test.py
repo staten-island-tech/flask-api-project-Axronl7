6a4a1d5106e60adfbd001a5b33d61917869931d1
Axronl7 <Aaronliu657@gmail.com>
2025-06-02 10:15:54 -0400

    :>

--- test.py
+++ test.py
@@ -1,6 +1,13 @@
 import requests
-response = requests.get("https://pokeapi.co/api/v2/pokemon?limit=1")
-data = response.json()
-pokemon_list = data['results']
-for pokemon in pokemon_list:
-    print(pokemon['name'])
+
+response = requests.get("https://raw.githubusercontent.com/WhalenSITHS/Pokedex-Python-Starter/main/pokedex.json")
+data = response.json()  
+
+
+for pokemon in data:
+    if 'Bug' in pokemon['type'] and 'Fire' in pokemon['type']:
+        print(pokemon['name']['japanese']) 
+
+for pokemon in data:
+    if pokemon['base']['HP'] > 65:
+        print(pokemon['name']['english'])
