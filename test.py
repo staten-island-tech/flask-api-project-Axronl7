dacd957306a1f6edd131c1d5d7bdca20fe84ce81
Axronl7 <Aaronliu657@gmail.com>
2025-06-03 09:49:22 -0400

    Revert ":>"
    This reverts commit 6a4a1d5106e60adfbd001a5b33d61917869931d1.

--- test.py
+++ test.py
@@ -1,13 +1,6 @@
 import requests
-
-response = requests.get("https://raw.githubusercontent.com/WhalenSITHS/Pokedex-Python-Starter/main/pokedex.json")
-data = response.json()  
-
-
-for pokemon in data:
-    if 'Bug' in pokemon['type'] and 'Fire' in pokemon['type']:
-        print(pokemon['name']['japanese']) 
-
-for pokemon in data:
-    if pokemon['base']['HP'] > 65:
-        print(pokemon['name']['english'])
+response = requests.get("https://pokeapi.co/api/v2/pokemon?limit=1")
+data = response.json()
+pokemon_list = data['results']
+for pokemon in pokemon_list:
+    print(pokemon['name'])
